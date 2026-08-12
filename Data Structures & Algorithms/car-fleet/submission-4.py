f55efa5022a973b3d6997a67cb4e6c99e744282a
dylanachen <162939222+dylanachen@users.noreply.github.com>
2026-08-12 16:46:30 -0700

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [[p, s] for p, s in zip(position, speed)]
+        cars = sorted(cars, reverse=True)
+        fleets = []
+
+        # traversing backwards
+        for p, s in cars:
+            # adding car's arrival time to the stack
+            fleets.append((target - p) / s)
+
+            # if the arrival time is before an existing arrival time, they must have collided and become a fleet
+            if len(fleets) > 1 and fleets[-1] <= fleets[-2]:
+                fleets.pop()
+            
+        return len(fleets)
