cdcea04c990805401f602731c964889f3c6eff68
dylanachen <162939222+dylanachen@users.noreply.github.com>
2026-08-13 16:22:09 -0700

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Use a pointer and a delayed pointer that we only set off after the first pointer has traversed n nodes
+        # By the time the pointer has reached the end, the delayed pointer will be n from the end
+        # If f.next == None, re-point the curr to next.next before moving the delayed pointer at all
+        if head.next == None:
+            return ListNode(val="")
+        
+        dummy = ListNode()
+        dummy.next = head
+        pointer = dummy
+        delayed = dummy
+
+        for _ in range(n):
+            print(pointer.val, pointer.next.val)
+            pointer = pointer.next
+
+        while pointer and pointer.next:            
+            print(pointer.val, pointer.next.val, delayed.val, delayed.next.val)
+            pointer = pointer.next
+            delayed = delayed.next
+        
+        delayed.next = delayed.next.next
+
+        return dummy.next
+
+        # n = 2 (+1)
+        # [0,1,2,3,4]
+        #  f
+        #    f
+        #      f
+        #  d     f
+        #    d     f.next = None
+        #    d.next = d.next.next
