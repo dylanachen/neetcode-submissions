bcfddb50194a3ddd3e79624e8fb129f25883eb22
dylanachen <162939222+dylanachen@users.noreply.github.com>
2026-08-15 23:53:40 -0700

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # when we add a value that overflows, we need to consider it in the addition of the next position of nodes
+        res = ListNode()
+        head = res
+        extra = 0
+
+        while l1 or l2 or extra == 1:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + extra
+
+            if val >= 10:
+                extra = 1
+                res.val = val - 10
+            else:
+                res.val = val
+                extra = 0
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+            if l1 or l2 or extra == 1:
+                res.next = ListNode()
+                res = res.next
+
+        return head
