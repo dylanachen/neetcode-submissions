3bc2ac854d5e86a86b68de1403ddbae0022bf942
dylanachen <162939222+dylanachen@users.noreply.github.com>
2026-08-25 23:32:03 -0700

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq_map = {i:[] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            prereq_map[course].append(prereq)
+        
+        visit_set = set()
+        in_path = set()
+        result = []
+
+        def dfs(course):
+            if course in in_path:
+                return result
+            if course in visit_set:
+                return []
+            # if prereq_map[course] == []:
+            #     result.append(course)
+
+            visit_set.add(course)
+            for prereq in prereq_map[course]:
+                if dfs(prereq) == []:
+                    return []
+            
+            visit_set.remove(course)
+            prereq_map[course] = []
+            in_path.add(course)
+
+            return result.append(course)
+        
+        for course in range(numCourses):
+            if dfs(course) == []:
+                return []
+            
+        return result
